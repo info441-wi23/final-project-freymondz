2581f9decf1f80a1edb447756620c46f5fc00a73
Zachary <37636251+freymondz@users.noreply.github.com>
2023-02-17 20:44:25 -0800

    basic test of auth (#4)

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import 'dotenv/config.js';
 
 import logger from 'morgan';
 import express from 'express';
+import session from 'express-session';
+import { WebAppAuthClientBuilder } from 'microsoft-identity-express';
 
 import models from './models.js';
 import apiv1 from './routes/api/v1/api.js';
@@ -12,6 +14,29 @@ app.use(express.json());
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+    }
+}));
+
+const appSettings = {
+    appCredentials: {
+        clientId: process.env.CLIENT_ID,
+        tenantId: process.env.TENAT_ID,
+        clientSecret: process.env.CLIENT_SECRET
+    },
+    authRoutes: {
+        redirect: 'http://localhost:3001/redirect',
+        error: '/error',
+        unauthorized: '/unauthorized'
+    }
+};
+const authClient = new WebAppAuthClientBuilder(appSettings).build();
+app.use(authClient.initialize());
 
 app.use((req, res, next) => {
     req['models'] = models;
@@ -19,8 +44,24 @@ app.use((req, res, next) => {
 });
 app.use('/api/v1', apiv1);
 
-app.listen(3001, "127.0.0.1", () => {
-    console.log('Server is running on http://127.0.0.1:3001/');
+app.get('/login',
+    authClient.signIn({ postLoginRedirect: '/' })
+);
+
+app.get('/logout',
+    authClient.signOut({ postLogoutRedirect: '/' })
+);
+
+app.get('/error', (req, res) => {
+    res.status(500).send("Error: Server error");
+});
+
+app.get('/unauthorized', (req, res) => {
+    res.status(401).send("Error: Unauthorized");
+});
+
+app.listen(3001, "localhost", () => {
+    console.log('Server is running on http://localhost:3001/');
 });
 
 export default app;
