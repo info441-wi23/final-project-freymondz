ca6046c893275ee52af322f5e0686b27bb2a2980
freymond <37636251+freymondz@users.noreply.github.com>
2023-03-03 23:22:32 -0800

    initial scaffold for profile page

--- public/javascripts/signin.js
+++ public/javascripts/signin.js
@@ -1,14 +1,22 @@
 window.addEventListener('load', async () => {
     const response = await fetch('/api/v1/users/identity');
     const data = await response.json();
-    const holder = document.querySelector('.loginButton');
+    const wrapper = document.querySelector('.wrapper');
     console.log(data)
     if (data.data) {
+        const holder = document.querySelector('.loginButton');
+        const profile = document.createElement('a');
+        profile.classList.add('profile');
+        profile.href = '/profile.html';
+        profile.innerText = data.data.user;
+
         const logout = document.createElement('a');
         logout.classList.add('loginButton');
         logout.href = 'logout';
         logout.innerText = 'Sign Out';
 
-        holder?.replaceWith(logout);
+        wrapper?.removeChild(holder)
+        wrapper?.appendChild(profile);
+        wrapper?.appendChild(logout);
     }
 });
