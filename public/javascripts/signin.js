b61a9d0d043a35659e72c54f94a5a5b6c8a63a0d
Zachary <37636251+freymondz@users.noreply.github.com>
2023-03-05 22:12:31 -0800

    Profile Page (#19)
    initial profile page

--- public/javascripts/signin.js
+++ public/javascripts/signin.js
@@ -1,14 +1,25 @@
 window.addEventListener('load', async () => {
     const response = await fetch('/api/v1/users/identity');
     const data = await response.json();
-    const holder = document.querySelector('.loginButton');
-    console.log(data)
+    const wrapper = document.querySelector('.wrapper');
     if (data.data) {
+        const holder = document.querySelector('.loginButton');
+        const profile = document.createElement('a');
+        profile.classList.add('profile');
+        profile.href = `profile.html?username=${data.data.user}`;
+        profile.innerText = data.data.user;
+
         const logout = document.createElement('a');
         logout.classList.add('loginButton');
         logout.href = 'logout';
         logout.innerText = 'Sign Out';
 
-        holder?.replaceWith(logout);
+        wrapper?.removeChild(holder);
+        wrapper?.appendChild(profile);
+        wrapper?.appendChild(logout);
+
+        await fetch('/api/v1/users/login', {
+            method: 'POST',
+        });
     }
 });
