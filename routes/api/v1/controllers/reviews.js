6aeb9ad42b303f5e2097511378f6cde1b333c027
Jeffrey Qiu <hongjie.qiu1@gmail.com>
2023-03-07 14:19:30 -0800

    updated detail page

--- routes/api/v1/controllers/reviews.js
+++ routes/api/v1/controllers/reviews.js
@@ -3,45 +3,58 @@ var router = express.Router();
 
 // get all shows stored in the MongoDB if the keyword is empty
 router.get('/', async (req, res) => {
-    try {
-      let showId = req.query.showId;
-      let showReviews = await req.models.review.find({ showId: showId });
-      let reviews = showReviews.map(review => 
-        `<div class="review-item">
-          <p>Username: ${review.username}</p>
-          <p>Season: ${review.season}</p>
-          <p>Episode: ${review.episode}</p>
-          <p>Rating: ${review.rating}</p>
-          <p>Review: ${review.review}</p>
-        </div>`
-      );
-      res.send(reviews.join(''));
-    } catch (error) {
-      res.status(500).send({ status: 'error', error: error });
+  try {
+    let showId = req.query.showId;
+    let showReviews = await req.models.review.find({ showId: showId });
+    if (showReviews == ' ') {
+      return `<div class="review-item"> No reviews for this show yet! </div>`
     }
-  });
 
-// This router will add new reviews to the mongodb reviews schema
-// not sure how to get showID
+    let reviews = showReviews.map(review => {
+      let reviewHTML = '';
+      if (review.username) {
+        reviewHTML += `<p>Username: ${review.username}</p>`;
+      }
+      if (review.season) {
+        reviewHTML += `<p>Season: ${review.season}</p>`;
+      }
+      if (review.episode) {
+        reviewHTML += `<p>Episode: ${review.episode}</p>`;
+      }
+      if (review.rating) {
+        reviewHTML += `<p>Rating: ${review.rating}</p>`;
+      }
+      if (review.review) {
+        reviewHTML += `<p>Review: ${review.review}</p>`;
+      }
+      return `<div class="review-item">${reviewHTML}</div>`;
+    });
+    res.send(reviews.join(''));
+  } catch (error) {
+    res.status(500).send({ status: 'error', error: error });
+  }
+});
+
 router.post('/', async (req, res) => {
-    console.log(req.body);
-    try {
-      const newReview = new req.models.review({
-        username: req.session.account?.username,
-        showId: req.body.showId,
-        review: req.body.review,
-        season: parseInt(req.body.season),
-        episode: parseInt(req.body.episode),
-      });
-  
-      await newReview.save();
-      console.log('New review added to schema');
-      res.json({ status: 'success' });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send({ status: 'error', error: error });
-    }
-  });
-  
+  console.log(req.body);
+  try {
+    const username = req.session.account?.name || 'Guest';
+    const newReview = new req.models.review({
+      username: username,
+      showId: req.body.showId,
+      review: req.body.review,
+      season: req.body.season,
+      rating: req.body.rating,
+      episode: req.body.episode,
+    });
+
+    await newReview.save();
+    console.log('New review added to schema');
+    res.json({ status: 'success' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ status: 'error', error: error });
+  }
+});
 
 export default router;
